d33c0a661a4103c03b12eb2087c3a40cd504be41
Susann Vorberg <susann.vorberg@gmail.com>
2017-02-09 14:03:33 +0100

    Update the book

--- _deploy.sh
+++ _deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+#build html book
+/usr/bin/Rscript -e "bookdown::render_book('/home/vorberg/Documents/phd_thesis/docs/index.Rmd', 'bookdown::gitbook(lib_dir = "libs")')"
+
+#commit and push to github
 git add --all *
 git commit -m "Update the book" 
 git push 
