a392148114d69198e98d5e98c1c7a67c3dd4c2f0
Susann Vorberg <susann.vorberg@gmail.com>
2017-07-04 21:26:40 +0200

    Update the book

--- _deploy.sh
+++ _deploy.sh
@@ -4,7 +4,7 @@
 Rscript -e "bookdown::render_book('/home/vorberg/Documents/phd_thesis/index.Rmd', 'bookdown::gitbook', output_dir='/home/vorberg/Documents/phd_thesis/docs')"
 
 #build pdf book
-/usr/bin/pandoc --latex-engine=xelatex --template latex/phd_thesis_template.tex --natbib --variable tables=yes --standalone --bibliography=book.bib 01-summary.md 02-acknowledgements.md toc.md 03-intro.md 04-methods.md  -o pdf/PhD_thesis_Susann_Vorberg.pdf
+/usr/bin/pandoc --latex-engine=xelatex --top-level-division=chapter --template latex/phd_thesis_template.tex --natbib --variable tables=yes --standalone --bibliography=book.bib 01-summary.md 02-acknowledgements.md toc.md 03-intro.md 04-methods.md  -o pdf/PhD_thesis_Susann_Vorberg.pdf
 
 #Rscript -e "bookdown::render_book('/home/vorberg/Documents/phd_thesis/index.Rmd', 'bookdown::pdf_book', output_dir='/home/vorberg/Documents/phd_thesis/pdf')"
 
