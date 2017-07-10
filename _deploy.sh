25da85c2e0837ae56183d47148903a027f6cc8be
Susann Vorberg <susann.vorberg@gmail.com>
2017-07-10 16:10:23 +0200

    Update the book

--- _deploy.sh
+++ _deploy.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-#build html book
-Rscript -e "bookdown::render_book('~/Documents/phd_thesis/index.Rmd', 'bookdown::gitbook', output_dir='~/Documents/phd_thesis/docs')"
-
-#build pdf book
-pandoc --latex-engine=xelatex --top-level-division=chapter --template latex/phd_thesis_template.tex --natbib --variable tables=yes --standalone --bibliography=book.bib 01-summary.md 02-acknowledgements.md toc.md 03-intro.md 04-methods.md  -o pdf/PhD_thesis_Susann_Vorberg.pdf
-
-#Rscript -e "bookdown::render_book('/home/vorberg/Documents/phd_thesis/index.Rmd', 'bookdown::pdf_book', output_dir='/home/vorberg/Documents/phd_thesis/pdf')"
-
+bash _deploy.sh
+ 
 #commit and push to github
 git add --all *
 git commit -m "Update the book" 
