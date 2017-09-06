356c10e2a916606d2f27cb3ecf30efcf95673a63
Susann Vorberg <susann.vorberg@gmail.com>
2017-09-06 12:16:44 +0200

    pdf works with static placeholder images for interactive html

--- _build.sh
+++ _build.sh
@@ -1,7 +1,12 @@
-#build html book
-Rscript -e "bookdown::render_book('~/Documents/phd_thesis/index.Rmd', 'bookdown::gitbook', output_dir='~/Documents/phd_thesis/docs')"
+
 
 #build pdf book
 #pandoc --latex-engine=xelatex --top-level-division=chapter --template latex/phd_thesis_template.tex --standalone --bibliography=bib/structure_prediction.bib 01-summary.md 02-acknowledgements.md toc.md 03-intro.md 04-methods.md  -o pdf/PhD_thesis_Susann_Vorberg.pdf
 
 Rscript -e "bookdown::render_book('~/Documents/phd_thesis/index.Rmd', 'bookdown::pdf_book', output_dir='~/Documents/phd_thesis/pdf')"
+
+#build html book
+rm -rf ~/Documents/phd_thesis/docs/*
+cp ~/Documents/phd_thesis/pdf/*pdf ~/Documents/phd_thesis/docs/
+Rscript -e "bookdown::render_book('~/Documents/phd_thesis/index.Rmd', 'bookdown::gitbook', output_dir='~/Documents/phd_thesis/docs')"
+
--- _deploy.sh
+++ _deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+commit_message=$1
+
 bash _build.sh
  
 #commit and push to github
 git add --all *
-git commit -m "Update the book" 
+git commit -m $commit_message 
 git push 
