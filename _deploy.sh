04e95cf882d883726c52a6dddcd7c5317e455afe
Susann Vorberg <susann.vorberg@gmail.com>
2017-09-30 20:11:53 +0200

    new full likelihood cd gd alpha0 precision vs rank plot

--- _deploy.sh
+++ _deploy.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-commit_message=$1
+commitmessage=$1
 
 bash _build.sh
  
 #commit and push to github
 git add --all *
 git add -f docs/PhD_thesis_Susann_Vorberg.pdf
-git commit -m $commit_message 
+git commit -m $commitmessage 
 git push 
